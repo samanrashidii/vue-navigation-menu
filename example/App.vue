5d168891faaea30301815a90d4851087323156ff
Saman Rashidi <samanrashidi44@gmail.com>
2020-03-16 12:27:59 +0330

    feature: created component structure

--- example/App.vue
+++ example/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    Vue Navigation Menu
+    <VueNavigationMenu />
   </div>
 </template>
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import VueNavigationMenu from "./VueNavigationMenu.vue";
 
 export default {
-  install(Vue, options) {
+  install(Vue) {
     Vue.component("vue-navigation-menu", VueNavigationMenu)
   }
 }
